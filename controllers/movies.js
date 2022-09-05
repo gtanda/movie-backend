bbb29b15b9920e6d9a70b3c99e4cd4acd9df4bf5
gtanda <govind_tanda1@hotmail.com>
2022-09-04 21:47:22 -0700

    fixed movies.js to use session user

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,11 +2,11 @@ const moviesRouter = require('express').Router();
 const User = require('../models/user');
 
 moviesRouter.post('/', async (req, res) => {
-    const user = req.body.user;
-    const userInDB = await User.findById(user.id);
+    const userId = req.session.user.userId;
+    const userInDB = await User.findById(userId);
 
     if (!userInDB) {
-        return res.status(400).json({ error: 'Login to add movies to watchlist' });
+        return res.status(400).json({error: 'Login to add movies to watchlist'});
     }
 
     const video = req.body.data;
@@ -16,21 +16,37 @@ moviesRouter.post('/', async (req, res) => {
     if (savedData) {
         return res.status(201).json(savedData);
     }
-    return res.status(400).json({ error: 'Could not saved to watchlist' });
+    return res.status(400).json({error: 'Could not saved to watchlist'});
 });
 
 moviesRouter.put('/', async (req, res) => {
-    const user = req.body.user;
+    const userId = req.session.user.userId;
     const dataToRemove = req.body.dataToRemove;
-    const userInDB = await User.findById(user.id);
+    const userInDB = await User.findById(userId);
 
     if (!userInDB) {
-        return res.status(400).json({ error: 'Login to remove movies from watchlist' });
+        return res.status(400).json({message: 'Login to remove movies from watchlist', messageStatus: 'error'});
     }
 
     userInDB.watchList = userInDB.watchList.filter(data => data.id !== dataToRemove.id);
-    const savedData = await userInDB.save();
-    return res.status(201).json(savedData);
+    console.log(userInDB.watchList.length);
+    await userInDB.save();
+    return res.status(201).json({
+        message: `Removed ${dataToRemove.title || dataToRemove.name} from watchlist`,
+        messageStatus: 'success'
+    });
+});
+
+moviesRouter.get('/', async (req, res) => {
+    const userId = req.session.user.userId;
+    const userInDB = await User.findById(userId);
+    console.log(userInDB);
+
+    if (!userInDB) {
+        return res.status(400).json({error: 'Login to view watchlist'});
+    }
+
+    return res.status(200).json(userInDB.watchList);
 });
 
 module.exports = moviesRouter;
