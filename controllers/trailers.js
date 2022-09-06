bd8e76721015cc1228a42f725014629a0c323053
gtanda <govind_tanda1@hotmail.com>
2022-09-05 17:39:51 -0700

    fixed hitting youtube API for trailers

--- controllers/trailers.js
+++ controllers/trailers.js
@@ -5,21 +5,21 @@ const url = require('url');
 trailerRouter.get('/', async (req, res) => {
     const parsedUrl = new url.URL(req.url, 'http://localhost:3001');
 
-    const trailerInfo = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${parsedUrl + ' trailer'}&key=${process.env.YT_API}`);
-    const releaseYear = parsedUrl.searchParams.get('releaseYear');
-    const parsedTitle = parsedUrl.searchParams.get('title').split(' ').join('+');
+    const trailerInfo = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${parsedUrl.searchParams.get('title') + ' trailer'}&key=${process.env.YT_API}`);
 
-    console.log(parsedTitle, releaseYear);
+    const releaseYear = parsedUrl.searchParams.get('releaseDate').split('-')[0];
+    const parsedTitle = parsedUrl.searchParams.get('title').toLowerCase();
+
     for (const trailer of trailerInfo.data.items) {
         const trailerRelease = trailer.snippet.publishedAt.split('-')[0];
         const trailerTitle = trailer.snippet.title.replace(/[^\w\s]/gi, '').toLowerCase();
 
-        if (trailerTitle.includes(parsedTitle) && trailerRelease === releaseYear) {
+        if (trailerTitle.includes(parsedTitle) || Number(trailerRelease) === Number(releaseYear)) {
             return res.status(200).json(trailer);
         }
     }
 
-    return res.status(400).json({ error: 'could not find trailer' });
+    return res.status(400).json({error: 'could not find trailer'});
 });
 
 module.exports = trailerRouter;
