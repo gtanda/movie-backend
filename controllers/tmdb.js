a9c4a844ab22f5a2f5da3e56e1b3a7451c6d165d
gtanda <govind_tanda1@hotmail.com>
2022-09-14 16:57:03 -0700

    added test for trending data

--- controllers/tmdb.js
+++ controllers/tmdb.js
@@ -1,9 +1,10 @@
 const tmdbRouter = require('express').Router();
 const axios = require('axios');
 
-tmdbRouter.get('/trending', async (req, res) => {
+tmdbRouter.get('/', async (req, res) => {
     const searchType = req.url.split('=')[1];
     const trending = await axios.get(`https://api.themoviedb.org/3/trending/${searchType}/day?api_key=${process.env.TMDB_API}`);
+    console.log('trending data', trending.data);
     return res.status(200).json(trending.data);
 });
 
--- tests/movie-retrival.test.js
+++ tests/movie-retrival.test.js
@@ -6,8 +6,10 @@ const api = supertest(app);
 
 describe('Movie retrieval ', () => {
     test('getting trending movies', async () => {
-        await api.get('/api/movies')
+        await api.get('/api/tmdb?searchType=movie')
             .expect(200)
             .expect('Content-Type', /application\/json/);
     });
 });
+
+afterAll(() => mongoose.connection.close());
