06f15b4fd9fd8a29da86fcdd5df8d9a23c38c236
gtanda <govind_tanda1@hotmail.com>
2022-11-17 20:10:10 -0800

    setting up route for fetching recommendation list

--- controllers/tmdb.js
+++ controllers/tmdb.js
@@ -11,9 +11,18 @@ tmdbRouter.get('/search', async (req, res) => {
     const query = req.url.split('=')[1].trim();
     const search = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API}&query=${query}`);
     if (search.data.results.length === 0) {
-        return res.status(404).json({ message: 'No results found'});
+        return res.status(404).json({ message: 'No results found' });
     }
     return res.status(200).json(search.data);
+});
+
+tmdbRouter.get('/recommendations/:id', async (req, res) => {
+    const recommendations = await axios.get(`https://api.themoviedb.org/3/movie/${req.params.id}/recommendations?api_key=${process.env.TMDB_API}`);
+    if (recommendations.data.results.length === 0) {
+        return res.status(404).json({ message: 'No recommendations found' });
+    }
+    console.log('recommendations', recommendations.data);
+    return res.status(200).json(recommendations.data);
 });
 
 module.exports = tmdbRouter;
